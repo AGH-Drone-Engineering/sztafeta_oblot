18996ba92a6acf0e9643645ff6e8c830aa769b69
Bartosz Bartoszewski <bartosz.bartoszewski@reakto.eu>
2024-09-27 00:19:08 +0200

    sh

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Funkcja, która zostanie uruchomiona, gdy skrypt otrzyma sygnał zakończenia
+cleanup() {
+    echo "Zamykanie aplikacji..."
+    # Zabijanie wszystkich uruchomionych w tle procesów
+    pkill -P $$
+    echo "Wszystkie procesy zostały zakończone."
+    exit 0
+}
+
+# Ustawienie pułapki na sygnały SIGINT (Ctrl+C) oraz SIGTERM
+trap cleanup SIGINT SIGTERM
+
 # Uruchomienie aplikacji Streamlit
 echo "Uruchamianie aplikacji Streamlit..."
 streamlit run frontend/mission_upload.py &
@@ -10,5 +22,3 @@ python3 backend/all_backend/backend.py &
 
 # Oczekiwanie na zakończenie wszystkich procesów
 wait
-
-echo "Wszystkie aplikacje zostały uruchomione."
